40c13b38870a807b1a5ecb0d6441f7a63df165c4
Matheus bertolli <matheusbertollianastacio@gmail.com>
2026-04-10 13:44:18 +0100

    Update

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 from fastapi import FastAPI, HTTPException, status, Response
-from typing import Dict
-from models import Course
+from typing import Dict, List
+from models import Course, courses
 
-app = FastAPI()
 
-#DB fake
-courses: Dict[int, dict] = {
-    1: {
-        "title": "Programming for laymen",
-        "course_classes": 112,
-        "hours": 55
-        },
-    2: {
-        "title": "Algorithm and programming logic",
-        "course_classes": 87,
-        "hours": 67 
-        },
-}
+app = FastAPI(
+    title='Courses API',
+    version='0.0.1',
+    description='An API for FastAPI studies'
+)
 
-@app.get('/courses', response_model=Dict[int, Course])
+
+@app.get('/courses', response_model=List[Course], description='Returns all courses or an empty list.', summary='Returns all courses.')
 async def get_courses():
     return courses 
 
-@app.get('/courses/{course_id}')
-async def get_curse(course_id: int):
+@app.get('/courses/{course_id}', description='Returns the course referenced by the ID.',
+         summary='It returns only one specific course.')
+async def get_course(course_id: int):
     try:
         course = courses[course_id]
         return course
@@ -31,7 +24,7 @@ async def get_curse(course_id: int):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='Course not found.')
     
-@app.post('/courses', status_code=status.HTTP_201_CREATED)
+@app.post('/courses',status_code=status.HTTP_201_CREATED, description='It allows you to create a course and integrate it into your list of courses.', summary='Create course.')
 async def post_course(course: Course):
     next_id = max(courses.keys()) + 1 if courses else 1
 
@@ -40,7 +33,7 @@ async def post_course(course: Course):
     courses[next_id] = course_data
     return course 
 
-@app.put('/courses/{course_id}')
+@app.put('/courses/{course_id}', description='Update an existing course in your list.', summary='Update course.')
 async def put_course(course_id: int, course: Course):
     if course_id in courses:
         course_data = course.dict()
@@ -52,7 +45,7 @@ async def put_course(course_id: int, course: Course):
             status_code=status.HTTP_404_NOT_FOUND, 
             detail=f'There is no course with the ID:{course_id}')
     
-@app.delete('/courses/{course_id}')
+@app.delete('/courses/{course_id}', description='Allows you to remove a course from your list.', summary='Delete course.')
 async def delete_course(course_id: int):
     if course_id in courses:
         del courses[course_id]
--- models.py
+++ models.py
@@ -6,3 +6,8 @@ class Course(BaseModel):
     title: str
     course_classes: int
     hours: int
+
+courses = [
+    Course(id=1, title='Programming for laymen', course_classes=42, hours=56),
+    Course(id=2, title='Algorithm and programming logic', course_classes=52, hours=67)
+]
